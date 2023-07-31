2c56c3c36c8ccd37a4eecc9af1598aa541bd3c8d
Natan <natangon10@gmail.com>
2023-07-31 01:03:47 -0300

    feat: tela de cadastro

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,39 +1,54 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApiService } from '../services/api.service';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent{
 
-  email = new FormControl('');
-  password= new FormControl('');
-  savePassword: boolean = false;
+  LoginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+    rememberMe: new FormControl(false),
+  });
 
-  constructor(private apiService: ApiService){
+
+  constructor(private apiService: ApiService, private router: Router){
 
   }
 
 
-  // onSubmit(){
-  //   console.log('onSubmit', this.email, this.password);
-  //   this.apiService.login(this.email, this.password).subscribe(
-  //     response => {
-  //       alert('Login successful');
-  //       console.log('Acesso Realizado com sucesso!', response)
-  //     },
-  //     error => {
-  //       console.error('Login failed!', error);
-  //       alert('Login failed!');
-  //     }
-  //   )
-  // }
+  onSubmit(){
+    const email: string = this.LoginForm.get('email')?.value ?? '';
+    const password: string = this.LoginForm.get('password')?.value ?? '';
+
+    if (!email || typeof email !== 'string') {
+      alert('Por favor, insira um email válido.');
+      return;
+    }
+
+    console.log("CHEGOU AQUI",email, password);
+
+    this.apiService.login(email, password).subscribe(
+      response => {
+        alert('Login successful');
+        console.log('Acesso Realizado com sucesso!', response)
+
+        this.router.navigate(['/feed']); //
+      },
+      error => {
+        console.error('Login failed!', error);
+        alert('Login failed!');
+      }
+    )
+  }
 
 
 }
