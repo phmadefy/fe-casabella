f52a19aa6e39f9ce92499621bcf85342274ef97a
Francisco Alex <franciscoalex2407@gmail.com>
2024-01-17 08:37:06 -0300

    fix design

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   url: 'https://api-cbella-01.azurewebsites.net',
   // url: 'http://localhost:8000',
-  api_key: 'FOin7CIBMMM6wQwqk9dPFQNoTHmnps2dShsvknot',
-  client_id: '9a1dbb3c-c092-4caa-9a33-c20becfab66c',
+  api_key: 'EAz5VCSWZQc7AHj3rp4GohMNoM2BDtdPZyfw1Dym',
+  client_id: '9b1bcf15-09e2-4876-9486-f70a2c7cfb88',
   token: 'casabellaToken',
 };
