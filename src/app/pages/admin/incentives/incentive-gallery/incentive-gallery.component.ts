988ac0defb867e1cb563a7f8cc490e4c9bdaffe4
Francisco Alex <franciscoalex2407@gmail.com>
2024-02-08 10:33:06 -0300

    fix: gallery incentive

--- src/app/pages/admin/incentives/incentive-gallery/incentive-gallery.component.ts
+++ src/app/pages/admin/incentives/incentive-gallery/incentive-gallery.component.ts
@@ -5,11 +5,22 @@ import { ToolsService } from 'src/app/services/tools.service';
 import { CardImageComponent } from 'src/app/components/card-image/card-image.component';
 import { RouterLink } from '@angular/router';
 import { BtnLikeComponent } from 'src/app/components/btn-like/btn-like.component';
+import { SpinnerComponent } from 'src/app/components/spinner/spinner.component';
+import { CardComponent } from 'src/app/components/card/card.component';
+import { Dialog } from '@angular/cdk/dialog';
+import { ModalSlideGalleryComponent } from 'src/app/shared/modal-slide-gallery/modal-slide-gallery.component';
 
 @Component({
   selector: 'app-incentive-gallery',
   standalone: true,
-  imports: [CommonModule, CardImageComponent, RouterLink, BtnLikeComponent],
+  imports: [
+    CommonModule,
+    CardImageComponent,
+    RouterLink,
+    BtnLikeComponent,
+    SpinnerComponent,
+    CardComponent,
+  ],
   providers: [ApiService],
   templateUrl: './incentive-gallery.component.html',
   styleUrls: ['./incentive-gallery.component.scss'],
@@ -20,9 +31,13 @@ export class IncentiveGalleryComponent {
 
   filters: any = { per_page: 50, page: 1 };
 
-  viewMode = false;
+  gallery: any = {};
 
-  constructor(private service: ApiService, public tools: ToolsService) {
+  constructor(
+    private service: ApiService,
+    public tools: ToolsService,
+    private dialog: Dialog
+  ) {
     service.path = 'v1/incentives-gallery';
   }
 
@@ -38,5 +53,26 @@ export class IncentiveGalleryComponent {
         this.dataSource = res;
       })
       .finally(() => (this.loading = false));
+  }
+
+  openView(item: any) {
+    this.loading = true;
+    this.service
+      .show(item.id)
+      .then((res) => {
+        this.gallery = res;
+      })
+      .finally(() => (this.loading = false));
+  }
+
+  openSlideGallery(index: number, images: any[]) {
+    const dialogRef = this.dialog.open<any>(ModalSlideGalleryComponent, {
+      // width: '95%',
+      maxWidth: '550px',
+      height: '90%',
+      maxHeight: '600px',
+      data: { active: index, images },
+      disableClose: true,
+    });
   }
 }
---
+++ src/app/shared/modal-slide-gallery/modal-slide-gallery.component.ts
@@ -0,0 +1,55 @@
+import { Component, Inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  SlickCarouselComponent,
+  SlickCarouselModule,
+} from 'ngx-slick-carousel';
+import { ApiService } from 'src/app/services/api.service';
+import { ToolsService } from 'src/app/services/tools.service';
+import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
+
+@Component({
+  selector: 'app-modal-slide-gallery',
+  standalone: true,
+  imports: [CommonModule, SlickCarouselModule],
+  providers: [ApiService],
+  templateUrl: './modal-slide-gallery.component.html',
+  styleUrls: ['./modal-slide-gallery.component.scss'],
+})
+export class ModalSlideGalleryComponent {
+  slideConfig = {
+    // slidesToShow: 1,
+    // arrows: false,
+    // dots: true,
+    // adaptiveHeight: true,
+    // respondTo: 'slider',
+    // autoplay: false,
+    // infinite: false,
+    // speed: 300,
+    // centerMode: true,
+    // variableWidth: true,
+  };
+
+  dataSource: any[] = [];
+  active = 0;
+  loading = false;
+
+  constructor(
+    private service: ApiService,
+    public tools: ToolsService,
+    public dialogRef: DialogRef,
+    @Inject(DIALOG_DATA) public data: any
+  ) {}
+
+  ngOnInit(): void {
+    this.dataSource = this.data?.images ?? [];
+    this.active = this.data?.active ?? 0;
+  }
+
+  prev(slickModal: SlickCarouselComponent) {
+    slickModal.slickPrev();
+  }
+  next(slickModal: SlickCarouselComponent) {
+    slickModal.slickNext();
+  }
+}
